97c7556aed202be369d4222d75c07175fd75e274
ted <ted@warpedpuppy.com>
2018-02-28 15:18:51 -0800

    asdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 const express = require('express');
 const path = require('path');
-const app = express()
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen('3000', () => console.log('listening. . . .'))
