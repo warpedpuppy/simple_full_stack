9f3335e36c143bca72bc8552443707dfad69ea92
ted <ted@warpedpuppy.com>
2018-02-28 16:49:43 -0800

    asdf

--- server.js
+++ server.js
@@ -2,5 +2,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname)
+app.get('/', (req, res) => {
+	res.send('okely dokely')
+})
+//app.use(express.static(path.join(__dirname, 'public')));
 app.listen('3000', () => console.log('listening. . . .'))
