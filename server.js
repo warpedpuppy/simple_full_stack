0124bbc2e413fa194f03fce1746ca113ae3923ce
ted <ted@warpedpuppy.com>
2018-03-07 19:29:34 -0800

    added routes

--- server.js
+++ server.js
@@ -2,5 +2,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+var users = require('./routes/users');
+app.use('/users', users);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(process.env.PORT || 5000, () => console.log('listening. . . .'))
